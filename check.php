7bb4cdc10185d6af9654419d46ee290ba6b1b674
Jeff Bennett <the.jeff.bennett@gmail.com>
2013-12-07 14:44:29 -0500

    1.0

---
+++ check.php
@@ -0,0 +1,41 @@
+<?php
+require("src/CreditCardFraudDetection.php");
+
+$ccfs = new CreditCardFraudDetection;
+$h["license_key"] = "Jh3He0L8HGbV";
+
+// Required fields
+$h["i"] = $_POST["i"];             // set the client ip address
+$h["city"] = $_POST["city"];             // set the billing city
+$h["region"] = $_POST["region"];                 // set the billing state
+$h["postal"] = $_POST["postal"];             // set the billing zip code
+$h["country"] = $_POST["country"];               // set the billing country
+
+// Recommended fields
+$h["domain"] = $_POST["domain"];		// Email domain
+$h["bin"] = $_POST["bin"];		// bank identification number
+$h["shipAddr"] = $_POST["shipAddr"];        // Shipping Address
+// CreditCardFraudDetection.php will take
+// MD5 hash of e-mail address passed to emailMD5 if it detects '@' in the string
+$h["emailMD5"] = $_POST["emailMD5"];
+// CreditCardFraudDetection.php will take the MD5 hash of the username/password if the length of the string is not 32
+
+$ccfs->timeout = 10;
+
+
+$ccfs->isSecure = 0;
+
+$ccfs->input($h);
+
+$ccfs->query();
+$h = $ccfs->output();
+
+$outputkeys = array_keys($h);
+$numoutputkeys = count($h);
+for ($i = 0; $i < $numoutputkeys; $i++) {
+    $key = $outputkeys[$i];
+    $value = $h[$key];
+    print $key . " = " . $value . "\n";
+    echo "<br>";
+}
+?>
---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<title>MaxMind Fraud Check Form</title>
+</head>
+<body>
+<form action="check.php" method="post">
+    Client IP Address: <input type="text" name="i"><br>
+    City: <input type="text" name="city"><br>
+    Region: <input type="text" name="region"><br>
+    Postal Code/ ZIP: <input type="text" name="postal"><br>
+    Country: <input type="text" name="country"><br>
+    Address: <input type="text" name="shipAddr"><br>
+    Domain: <input type="text" name="domain"><br>
+    Credit Card Bin: <input type="text" name="bin"><br>
+    Email Address: <input type="text" name="emailMD5"><br>
+    <input type="submit">
+</form>
+</body>
+</html>
